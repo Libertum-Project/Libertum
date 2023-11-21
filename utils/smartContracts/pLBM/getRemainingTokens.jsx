d94e469bb732ece4e5d496a1733b8031206c502e
madhava <galarzaguillermo.ggm@gmail.com>
2023-11-21 17:07:41 -0300

    Fix (pLBM): Remaining token formatting precision

--- utils/smartContracts/pLBM/getRemainingTokens.jsx
+++ utils/smartContracts/pLBM/getRemainingTokens.jsx
@@ -1,34 +1,41 @@
-import { ethers } from 'ethers';
-import pLBM_ABI from '../ABI/pLBM.json';
+import { ethers } from "ethers";
+import pLBM_ABI from "../ABI/pLBM.json";
 const pLBM_address = process.env.NEXT_PUBLIC_pLBM_address;
 
-export async function getRemainingTokens(provider) {
+export async function getRemainingTokens() {
   try {
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://polygon-mumbai.g.alchemy.com/v2/1MGoef4uSxJ3hjS0wszW_hmrScMeLq6B",
+    );
     const contract = new ethers.Contract(pLBM_address, pLBM_ABI.abi, provider);
 
     const seedTokensRemaining = await contract.seedTokensRemaining();
     const whitelistTokensRemaining = await contract.whitelistTokensRemaining();
     const publicTokensRemaining = await contract.publicTokensRemaining();
 
-    const seedTokensRemainingUserFriendly = parseInt(
-      seedTokensRemaining / BigInt(10 ** 18),
-    );
-    const whitelistTokensRemainingUserFriendly = parseInt(
-      whitelistTokensRemaining / BigInt(10 ** 18),
-    );
-    const publicTokensRemainingUserFriendly = parseInt(
-      publicTokensRemaining / BigInt(10 ** 18),
+    const seedTokensRemainingUserFriendly = ethers.utils.formatUnits(
+      seedTokensRemaining,
+      18,
     );
 
+    const whitelistTokensRemainingUserFriendly = ethers.utils.formatUnits(
+      whitelistTokensRemaining,
+      18,
+    );
+
+    const publicTokensRemainingUserFriendly = ethers.utils.formatUnits(
+      publicTokensRemaining,
+      18,
+    );
     return {
       seedTokensRemaining: seedTokensRemainingUserFriendly,
       whitelistTokensRemaining: whitelistTokensRemainingUserFriendly,
       publicTokensRemaining: publicTokensRemainingUserFriendly,
     };
   } catch (error) {
-    console.error('Error fetching remaining tokens:', error);
+    console.error("Error fetching remaining tokens:", error);
     return {
-      error: 'Error fetching remaining tokens',
+      error: "Error fetching remaining tokens",
     };
   }
 }
