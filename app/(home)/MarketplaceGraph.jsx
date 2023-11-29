e2de2a905a65dc24896a6ca0abab02c1cf004a61
madhava <galarzaguillermo.ggm@gmail.com>
2023-11-29 15:30:20 -0300

    Fix Sync Issue

--- app/(home)/MarketplaceGraph.jsx
+++ app/(home)/MarketplaceGraph.jsx
@@ -4,9 +4,8 @@ export default function MarketplaceGraph() {
   return (
     <div className="lg:pt-16 pt-8">
       <h2 className="lg:py-12 py-4 text-center lg:text-6xl md:text-4xl text-2xl font-bold tracking-tight text-gray-900">
-      The Libertum platform is Designed with Simplicity at its Core.​      
+        The Libertum platform is Designed with Simplicity at its Core.
       </h2>
-
       <Image
         className="mix-blend-normal px-1 pt-0 pb-4 lg:py-16 lg:mx-auto lg:max-w-screen-2xl w-full"
         src="/img/marketplaceGraph.svg"
