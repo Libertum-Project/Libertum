9b925b3d3e46d97d5a4aad56a73769fd9a9bbcb8
nikkiburgos <burgosveganicole@gmail.com>
2023-12-08 01:05:06 -0300

    fixed sizes and design

--- app/(home)/LargeInfoSection.jsx
+++ app/(home)/LargeInfoSection.jsx
@@ -5,12 +5,12 @@ import { Slide } from "react-awesome-reveal";
 export default function LargeInfoSection() {
   return (
     <>
-      <div className="relative overflow-hidden bg-gray-100 bg-opacity-20 rounded-xl pt-16 pb-32 space-y-24 mt-6">
+      <div className="relative overflow-hidden bg-opacity-20 rounded-xl pt-16 pb-32 space-y-24 mt-6">
         {/* Primera Sección */}
-        <Slide direction={"left"} triggerOnce={false}>
+        <Slide direction={"left"} triggerOnce={true}>
           <div className="relative">
             <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
-              <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0">
+              <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-6 lg:px-0">
                 <div>
                   <div>
                     <span className="flex h-12 w-12 items-center justify-center rounded-xl bg-blue-500">
@@ -36,12 +36,12 @@ export default function LargeInfoSection() {
                       The next stage in the evolution of real estate investment
                       is here.
                     </h2>
-                    <p className="mt-4 text-lg text-gray-600">
+                    <p className="mt-4 text-base text-gray-600">
                       Libertum eliminates the need for intermediaries, making
                       real estate investment cost-effective and efficient,
                       benefiting both investors and property professionals.
                     </p>
-                    <p className="mt-4 text-lg text-gray-600">
+                    <p className="mt-4 text-base text-gray-600">
                       Libertum will unite small and large investors with
                       property owners on our global peer to peer platform,
                       granting local property investment opportunities global
@@ -51,12 +51,12 @@ export default function LargeInfoSection() {
                 </div>
               </div>
               <div className="mt-12 sm:mt-16 lg:mt-0">
-                <div className="-mr-48 pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+                <div className="-mr-48 pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0 object-fill">
                   <Image
                     loading="lazy"
-                    width={647}
-                    height={486}
-                    className="w-full rounded-xl shadow-2xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                    width={547}
+                    height={386}
+                    className="w-full rounded-xl shadow-2xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full  "
                     style={{ color: "transparent" }}
                     src="/assetsLanding/phoneMarketplace.png"
                     alt="Imagen 1"
@@ -68,10 +68,10 @@ export default function LargeInfoSection() {
         </Slide>
 
         {/* Segunda Sección */}
-        <Slide direction={"right"} triggerOnce={false}>
+        <Slide direction={"right"} triggerOnce={true}>
           <div className="relative">
             <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
-              <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0 lg:col-start-2">
+              <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-6 lg:px-0 lg:col-start-2">
                 <div>
                   <div>
                     <span className="flex h-12 w-12 items-center justify-center rounded-xl bg-blue-500">
@@ -96,11 +96,11 @@ export default function LargeInfoSection() {
                     <h2 className="text-3xl font-bold tracking-tight text-gray-600">
                       Simplifying Real Estate Investment
                     </h2>
-                    <p className="mt-4 text-lg text-gray-500">
+                    <p className="mt-4 text-base text-gray-500">
                       Goodbye to the complexities of ownership transfers and the
                       burden of unforeseen expenses.
                     </p>
-                    <p className="mt-4 text-lg text-gray-500">
+                    <p className="mt-4 text-base text-gray-500">
                       At <span className="text-bold">Libertum</span> we are
                       introducing a ground breaking approach to real estate
                       investment, fostering accessibility, liquidity, and
@@ -113,13 +113,13 @@ export default function LargeInfoSection() {
                 </div>
               </div>
               <div className="mt-12 sm:mt-16 lg:mt-0">
-                <div className="-ml-48 pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+                <div className="-ml-48 pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0 object-fill">
                   <Image
                     alt="Interfaz de usuario de correo electrónico"
                     loading="lazy"
-                    width={647}
-                    height={486}
-                    className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                    width={547}
+                    height={386}
+                    className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full "
                     style={{ color: "transparent" }}
                     src="/assetsLanding/housesblue.jpg"
                   />
@@ -129,10 +129,10 @@ export default function LargeInfoSection() {
           </div>
         </Slide>
         {/* Tercera Sección */}
-        <Slide direction={"left"} triggerOnce={false}>
+        <Slide direction={"left"} triggerOnce={true}>
           <div className="relative">
             <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
-              <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0 ">
+              <div className="mx-auto max-w-xl px-6 lg:mx-0 lg:max-w-none lg:py-6 lg:px-0 ">
                 <div>
                   <div>
                     <span className="flex h-12 w-12 items-center justify-center rounded-xl bg-blue-500">
@@ -157,7 +157,7 @@ export default function LargeInfoSection() {
                     <h2 className="text-3xl font-bold tracking-tight text-gray-700">
                       Staking Pool
                     </h2>
-                    <p className="mt-4 text-lg text-gray-600">
+                    <p className="mt-4 text-base text-gray-600">
                       We are different. 5% of our investment returns will be
                       dedicated to an ongoing buyback and burn strategy, aimed
                       at increasing the value of our tokens and benefiting early
@@ -167,7 +167,7 @@ export default function LargeInfoSection() {
                     <h2 className="text-3xl mt-6 font-bold tracking-tight text-gray-700">
                       Rental Income Token (RIT)
                     </h2>
-                    <p className="mt-4 text-lg text-gray-600">
+                    <p className="mt-4 text-base text-gray-600">
                       $LBM can be used to participate in Libertum fractionalised
                       rental income ecosystem. Use $LBM to purchase RITs and sit
                       back and enjoy rental income from premium properties. Let
@@ -177,12 +177,12 @@ export default function LargeInfoSection() {
                 </div>
               </div>
               <div className="mt-12 sm:mt-16 lg:mt-0">
-                <div className="-mr-48 pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0">
+                <div className="-mr-48 pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0 object-fill">
                   <Image
                     loading="lazy"
                     width={646}
                     height={485}
-                    className="w-full rounded-xl shadow-2xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                    className="w-full rounded-xl shadow-2xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto "
                     style={{ color: "transparent" }}
                     src="/assetsLanding/bussinessLaptop.jpg"
                     alt="Two people looking a chart in a laptop"
