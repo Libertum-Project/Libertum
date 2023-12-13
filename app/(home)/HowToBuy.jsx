24c41f16d95b7653c871b4dc6031fe1fb3246a99
nikkiburgos <burgosveganicole@gmail.com>
2023-12-13 09:49:59 -0300

    how to buy SET UP - NO MERGE YET

---
+++ app/(home)/HowToBuy.jsx
@@ -0,0 +1,39 @@
+// HowToBuy.js
+import styles from './HowToBuy.module.css';
+
+
+const HowToBuy = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardContainer}>
+
+      <div className={styles.card}>
+        <h2>1. Connect your wallet</h2>
+        {/* Contenido del primer paso */}
+      </div>
+
+      <div className={styles.card}>
+        <h2>2. Be sure it is connected to Polygon network</h2>
+        {/* Contenido del segundo paso */}
+      </div>
+
+      <div className={styles.card}>
+        <h2>3. Choose the amount of LBM you want to purchase</h2>
+        {/* Contenido del tercer paso */}
+      </div>
+
+      <div className={styles.card}>
+        <h2>4. Accept the transaction and buy!</h2>
+        {/* Contenido del cuarto paso */}
+      </div>
+
+      </div>      
+
+      <div className={styles.mockup}>
+        <img src="url_de_la_primera_imagen" alt="Mockup de teléfono" />
+      </div>
+    </div>
+  );
+};
+
+export default HowToBuy;
