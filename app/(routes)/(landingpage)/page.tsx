43a8251f68314aadcf46ed18479aed88cdf73af2
nikiburgos <burgosveganicole@gmail.com>
2024-01-18 14:30:20 -0300

    card folder

--- app/(routes)/(landingpage)/page.tsx
+++ app/(routes)/(landingpage)/page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from "./hero/Hero";
 import { BrandCarrousel } from "./brandsCarrousel/BrandCarrousel";
-import { Cards } from "../cards/Cards";
+import { Cards } from "./cards/Cards";
 import { Tags } from "./tags/Tags";
 import { Brands } from "./brands/Brands";
 
