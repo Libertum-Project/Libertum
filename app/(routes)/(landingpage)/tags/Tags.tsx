b9bfe5c8b97edee85fde9d98e13b4145419dd918
nikiburgos <burgosveganicole@gmail.com>
2024-01-18 17:50:45 -0300

    tags

--- app/(routes)/(landingpage)/tags/Tags.tsx
+++ app/(routes)/(landingpage)/tags/Tags.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { type ReactElement, useState, useEffect } from "react";
+import { type ReactElement, useState } from "react";
 import Image from "next/image";
 import css from "./Tags.module.css";
 import inactiveArrow from "./assets/inactiveArrow.svg";
@@ -25,99 +25,12 @@ interface Option {
   };
 }
 
-const options: Option[] = [
-  {
-    id: "realEstate",
-    icon: { active: realEstateACTIVE, inactive: realEstateINACTIVE },
-    name: "Real estate agencies and owners",
-    information: {
-      title: "Real Estate Information",
-      paragraphs: [
-        "Forward thinking real estate owners and agencies can be part of the Libertum evolution.",
-        "$35 trillion worth of assets will be tokenized within the next decade according to the World Economic Forum.",
-        "Don’t be left behind.",
-      ],
-      buttonLabel: "Learn More",
-    },
-  },
-  {
-    id: "ambassadors",
-    icon: { active: ambassadorsACTIVE, inactive: ambassadorsINACTIVE },
-    name: "Libertum Ambassadors",
-    information: {
-      title: "Libertum Ambassadors",
-      paragraphs: [
-
-        "Get rewarded by contributing to the growth of the LIbertum network.  Collaborate with Libertum to help drive global adoption.",
-        "We want to speak to influencers and marketers who understand the RWA landscape and want to contribute to the evolution happening in real estate tokenisation.  You will be eligible for various reward programmes designed to accelerate asset tokenisation globally ",
-      ],
-      buttonLabel: "Contact Us",
-    },
-  },
-  {
-    id: "investors",
-    icon: { active: investingACTIVE, inactive: investingINACTIVE },
-    name: "Smart Investors",
-    information: {
-      title: "Smart Investors",
-      paragraphs: [
-
-        "We are currently considering additional investors in Libertum. If you have experience in blockchain and real estate excite, if you understand the step-change that is taking place in real estate we want to partner with you.",
-        "Our to goal in collaborating is ensure to ensure mutual success - help us scale faster and leverage the cross network platform we have been building.",
-      ],
-      buttonLabel: "Reach Out",
-    },      
-  },
-  {
-    id: "partners",
-    icon: { active: partnersACTIVE, inactive: partnersINACTIVE },
-    name: "Technology Partners",
-    information: {
-      title: "Technology Partners",
-      paragraphs: [
-
-        "We aim to always be at the forefront of emerging technology.",
-        "If you are a technology provider that knows RWA tokenisation is the future evolution of investment contact us about how our community can benefit from integrating with your technology. ",
-      ],
-      buttonLabel: "Contact Us",
-    },      
-  },
-
-
-
-];
-
 export function Tags() {
   const [activeOption, setActiveOption] = useState("realEstate"); 
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 790);
-  
-  const handleResize = () => {
-    setIsMobile(window.innerWidth < 790);
-  };
-
-  const [expandedOptions, setExpandedOptions] = useState<string[]>([options[0].id]);
 
   const handleOptionClick = (optionId: string) => {
-    // Toggle the selected option
-    const newExpandedOptions = expandedOptions.includes(optionId)
-      ? expandedOptions.filter((id) => id !== optionId)
-      : [...expandedOptions, optionId];
-
     setActiveOption(optionId);
-    setExpandedOptions(newExpandedOptions);
   };
-
-
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  // const handleOptionClick = (optionId: string) => {
-  //   setActiveOption(optionId);
-  // };
 
   const handleOptionHover = (optionId: string) => {
     if (activeOption !== optionId) {
@@ -125,92 +38,117 @@ export function Tags() {
     }
   };
 
+  const options: Option[] = [
+    {
+      id: "realEstate",
+      icon: { active: realEstateACTIVE, inactive: realEstateINACTIVE },
+      name: "Real estate agencies and owners",
+      information: {
+        title: "Real Estate Information",
+        paragraphs: [
+          "Forward thinking real estate owners and agencies can be part of the Libertum evolution.",
+          "$35 trillion worth of assets will be tokenized within the next decade according to the World Economic Forum.",
+          "Don’t be left behind.",
+        ],
+        buttonLabel: "Learn More",
+      },
+    },
+    {
+      id: "ambassadors",
+      icon: { active: ambassadorsACTIVE, inactive: ambassadorsINACTIVE },
+      name: "Libertum Ambassadors",
+      information: {
+        title: "Libertum Ambassadors",
+        paragraphs: [
 
+          "Get rewarded by contributing to the growth of the LIbertum network.  Collaborate with Libertum to help drive global adoption.",
+          "We want to speak to influencers and marketers who understand the RWA landscape and want to contribute to the evolution happening in real estate tokenisation.  You will be eligible for various reward programmes designed to accelerate asset tokenisation globally ",
+        ],
+        buttonLabel: "Contact Us",
+      },
+    },
+    {
+      id: "investors",
+      icon: { active: investingACTIVE, inactive: investingINACTIVE },
+      name: "Smart Investors",
+      information: {
+        title: "Smart Investors",
+        paragraphs: [
+
+          "We are currently considering additional investors in Libertum. If you have experience in blockchain and real estate excite, if you understand the step-change that is taking place in real estate we want to partner with you.",
+          "Our to goal in collaborating is ensure to ensure mutual success - help us scale faster and leverage the cross network platform we have been building.",
+        ],
+        buttonLabel: "Reach Out",
+      },      
+    },
+    {
+      id: "partners",
+      icon: { active: partnersACTIVE, inactive: partnersINACTIVE },
+      name: "Technology Partners",
+      information: {
+        title: "Technology Partners",
+        paragraphs: [
+
+          "We aim to always be at the forefront of emerging technology.",
+          "If you are a technology provider that knows RWA tokenisation is the future evolution of investment contact us about how our community can benefit from integrating with your technology. ",
+        ],
+        buttonLabel: "Contact Us",
+      },      
+    },
+
+
+
+  ];
 
   return (
     <div className={css.container}>
       <h2 className={css.title}>Be part of the RWA Evolution. Today.</h2>
 
-      {isMobile ? (
-        // Render mobile version
-        <div className={css.tabAccordionMobile}>
-          <div className={css.options}>
-            {options.map((option) => (
-              <div key={option.id} className={css.tabPanelMobile}>
-                <div
-                  className={`${css.frameTitleMobile} ${expandedOptions.includes(option.id) ? css.active : css.inactive}`}
-                  onClick={() => handleOptionClick(option.id)}
-                >
-                  <h2>{option.name}</h2>
-                </div>
-
-                {expandedOptions.includes(option.id) && (
-                  <div className={css.informationMobile}>
-                    <div>
-                      <h2>{option.information.title}</h2>
-                      {option.information.paragraphs.map((paragraph, index) => (
-                        <p key={index}>{paragraph}</p>
-                      ))}
-                    </div>
-
-                    <button className={css.buttonInformation}>
-                      {option.information.buttonLabel}
-                      <Image alt="left arrow" src="/assets/leftArrowBLACK.svg" width={13.207} height={8.708} />
-                    </button>
-                  </div>
-                )}
+      <div className={css.tabAccordion}>
+        <section className={css.options}>
+          {options.map((option) => (
+            <div
+              key={option.id}
+              className={`${css.tabPanel} ${activeOption === option.id ? css.active : ""}`}
+              onClick={() => handleOptionClick(option.id)}
+              onMouseEnter={() => handleOptionHover(option.id)} 
+            >
+              <div className={css.optionsIcon}>
+                <Image
+                  alt="left arrow"
+                  src={activeOption === option.id ? option.icon.active : option.icon.inactive}
+                  width="24"
+                  height="24"
+                />
               </div>
+
+              <div className={`${css.frameTitle} ${activeOption !== option.id ? css.inactive : ""}`}>
+                <h2>{option.name}</h2>
+                <Image
+                  alt="left arrow"
+                  src={activeOption === option.id ? activeArrow : inactiveArrow}
+                  width="40"
+                  height="40"
+                />
+              </div>
+            </div>
+          ))}
+        </section>
+
+        <section className={css.information}>
+          <div>
+            <h2>{options.find((opt) => opt.id === activeOption)?.information.title}</h2>
+            {options.find((opt) => opt.id === activeOption)?.information.paragraphs.map((paragraph, index) => (
+              <p key={index}>{paragraph}</p>
             ))}
           </div>
-        </div>
-      ) : (
-        // Render desktop version
-        <div className={css.tabAccordion}>
-          <section className={css.options}>
-            {options.map((option) => (
-              <div
-                key={option.id}
-                className={`${css.tabPanel} ${activeOption === option.id ? css.active : ""}`}
-                onClick={() => handleOptionClick(option.id)}
-                onMouseEnter={() => handleOptionHover(option.id)} 
-              >
-                <div className={css.optionsIcon}>
-                  <Image
-                    alt="left arrow"
-                    src={activeOption === option.id ? option.icon.active : option.icon.inactive}
-                    width="24"
-                    height="24"
-                  />
-                </div>
 
-                <div className={`${css.frameTitle} ${activeOption !== option.id ? css.inactive : ""}`}>
-                  <h2>{option.name}</h2>
-                  <Image
-                    alt="left arrow"
-                    src={activeOption === option.id ? activeArrow : inactiveArrow}
-                    width="40"
-                    height="40"
-                  />
-                </div>
-              </div>
-            ))}
-          </section>
-
-          <section className={css.information}>
-            <div>
-              <h2>{options.find((opt) => opt.id === activeOption)?.information.title}</h2>
-              {options.find((opt) => opt.id === activeOption)?.information.paragraphs.map((paragraph, index) => (
-                <p key={index}>{paragraph}</p>
-              ))}
-            </div>
-
-            <button className={css.buttonInformation}>
-              {options.find((opt) => opt.id === activeOption)?.information.buttonLabel}
-              <Image alt="left arrow" src="/assets/leftArrowBLACK.svg" width={13.207} height={8.708} />
-            </button>
-          </section>
-        </div>
-      )}
+          <button className={css.buttonInformation}>
+            {options.find((opt) => opt.id === activeOption)?.information.buttonLabel}
+            <Image alt="left arrow" src="/assets/leftArrowBLACK.svg" width={13.207} height={8.708} />
+          </button>
+        </section>
+      </div>
     </div>
   );
 }
