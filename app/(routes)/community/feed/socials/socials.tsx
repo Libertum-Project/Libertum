cc95fb28ddcd24b3e2e012eeeaa0d862b9557710
nikiburgos <burgosveganicole@gmail.com>
2024-01-23 19:10:20 -0300

    mobile community

--- app/(routes)/community/feed/socials/socials.tsx
+++ app/(routes)/community/feed/socials/socials.tsx
@@ -40,6 +40,7 @@ export function Socials(): ReactElement {
     <div className={css.socialContainer}>
        {socials.map((social, index) => (
         <div key={index} className={css.socialCard}>
+          <div className={css.frameTitle}>
           <Image 
           src={social.icon} 
           alt={social.name}
@@ -47,12 +48,14 @@ export function Socials(): ReactElement {
           width="86" 
           />
           <h2 className={css.socialCardName}>{social.name}</h2>
+          </div>
+         
           <p className={css.socialCardInfo}> {social.info}</p>
           <Link 
           className={css.socialCardButton}
           href={social.buttonLink}
           >
-            CONNECT
+            Connect
             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="10" viewBox="0 0 14 10" fill="none">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M8.854 0.645996L13.207 5L8.854 9.354L8.146 8.646L11.293 5.5H0V4.5H11.293L8.146 1.354L8.854 0.645996Z" fill="#080915"/>
             </svg>
