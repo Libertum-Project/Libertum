c40c17bbaba516f8e48eda7e9a241c70ec75ebeb
madhava <galarzaguillermo.ggm@gmail.com>
2024-02-01 14:29:51 -0300

    Refactor(SmartContracts): getCurrentSaleStage TS

--- utils/smartContracts/pLBM/getRemainingTokens.ts
+++ utils/smartContracts/pLBM/getRemainingTokens.ts
@@ -1,47 +1,19 @@
 import { Contract, getDefaultProvider, formatUnits } from "ethers";
 import pLBM_ABI from "../ABI/pLBM.json";
 
-const pLBM_address: string | undefined = process.env.NEXT_PUBLIC_pLBM_address;
+const pLBM_address: string = process.env.NEXT_PUBLIC_pLBM_address!;
 const provider = getDefaultProvider(
   "https://polygon-mainnet.g.alchemy.com/v2/eYoj2pyMsQV15pjd0WADpjpY5TRJmucG",
 );
-const contract = new Contract(pLBM_address!, pLBM_ABI.abi, provider);
+const contract = new Contract(pLBM_address, pLBM_ABI.abi, provider);
 
-async function getSeedTokensRemaining(): Promise<string> {
-  const seedTokensRemaining = await contract.seedTokensRemaining();
-  return formatUnits(seedTokensRemaining, 18);
-}
-
-async function getWhitelistTokensRemaining(): Promise<string> {
-  const whitelistTokensRemaining = await contract.whitelistTokensRemaining();
-  return formatUnits(whitelistTokensRemaining, 18);
-}
-
-async function getPublicTokensRemaining(): Promise<string> {
-  const publicTokensRemaining = await contract.publicTokensRemaining();
-  return formatUnits(publicTokensRemaining, 18);
-}
-
-export async function getRemainingTokens(
-  stage: string,
-): Promise<string | undefined> {
+export async function getRemainingTokens(stage: string): Promise<string> {
   try {
-    switch (stage) {
-      case "seed":
-        const seedTokensRemaining = await getSeedTokensRemaining();
-        return parseFloat(seedTokensRemaining).toFixed(1);
-      case "whitelist":
-        const whitelistTokensRemaining = await getWhitelistTokensRemaining();
-        return parseFloat(whitelistTokensRemaining).toFixed(1);
-      case "public":
-        const publicTokensRemaining = await getPublicTokensRemaining();
-        return parseFloat(publicTokensRemaining).toFixed(1);
-      default:
-        console.error(`Invalid stage: ${stage}`);
-        throw new Error(`Invalid stage: ${stage}`);
-    }
+    const remainingTokens = await contract[`${stage}TokensRemaining`]();
+    const parsedRemainingTokens = parseFloat(formatUnits(remainingTokens, 18));
+    return parsedRemainingTokens.toFixed(1);
   } catch (error) {
-    console.error("Error fetching public tokens:", error);
-    throw Error;
+    console.error(`Error fetching remaining tokens for stage ${stage}:`, error);
+    throw new Error(`Failed to fetch remaining tokens for stage: ${stage}`);
   }
 }
