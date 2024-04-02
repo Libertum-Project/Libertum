7bee0b59b86584c8ff68d2ea60f47890522bf0e6
nikiburgos <burgosveganicole@gmail.com>
2024-04-02 14:36:25 -0300

    fixed mobile

--- app/(routes)/rfp/tags/TagsMobile.tsx
+++ app/(routes)/rfp/tags/TagsMobile.tsx
@@ -1,9 +1,9 @@
-"use client";
-import { type ReactElement, useState } from "react";
-import Image from "next/image";
-import css from "./TagsMobile.module.css";
-import Link from "next/link";
-import { ContactForm } from "../contactForm/contactFrom";
+'use client';
+import { type ReactElement, useState } from 'react';
+import Image from 'next/image';
+import css from './TagsMobile.module.css';
+import Link from 'next/link';
+import { ContactForm } from '../contactForm/contactFrom';
 
 interface Option {
   id: string;
@@ -27,7 +27,7 @@ export function TagsMobile() {
           'Libertum is currently seeking proposals for an <b>Indefinite Delivery/Indefinite Quantity</b> contract to provide engagement tools for our community. The solutionpartner will be required to develop and deploy sophisticated software Infrastructure for Decentralized Tokenized RWA real estate markets.',
           'The solution provider should have a proven ability to provide          community engagement solutions such as staking pools for both           liquidity and funding, and community engagement and transparency tools for Libertums LBM token and the subsequent Rental Income Token (RIT).',
           'Proposals will be evaluated using the best value trade-off method,    with non-price factors being significantly more important than price.',
-          '<b> Applications </b> are due by <i>April 5, 2024</i>, and <b>proposals</b> are due by <i>April 10, 2024</i>.',
+          '📅 <b> Applications </b> are due by <i>April 5, 2024</i>, and <b>proposals</b> are due by <i>April 10, 2024</i>.',
         ],
         buttonLabel: 'Contact Us',
       },
@@ -35,12 +35,15 @@ export function TagsMobile() {
     },
     {
       id: 'ambassadors',
-      name: 'Other Proposal',
+      name: 'Interoperability Protocol (UIP)',
       information: {
-        title: 'Other Proposal',
+        title: ' Interoperability Protocol (UIP)',
         paragraphs: [
-          'Get rewarded by contributing to the growth of the LIbertum network.  Collaborate with Libertum to help drive global adoption.',
-          'We want to speak to influencers and marketers who understand the RWA landscape and want to contribute to the evolution happening in real estate tokenisation.  You will be eligible for various reward programmes designed to accelerate asset tokenisation globally ',
+          'Libertum is currently seeking proposals for an Indefinite Delivery contract to provide <b>Interoperability Protocol (UIP)</b> tools for our community.',
+          'The solution partner will be required to develop and deploy sophisticated software Infrastructure to compliment the Decentralized Tokenized #RWA real estate Market Libertum provides.',
+          'The solution provider should have a proven ability to provide Liquidity Distribution Mechanisms solutions to enable the Libertum community to navigate on-chain data across multiple blockchains seemlessly. Ultimately the solution should simplify the users ability to interact with $LBM.',
+          'As always proposals will be evaluated using the best value trade-off method, with non-price factors being significantly more important than price.',
+          '📅 <b>Applications</b> are due by  <i>April 5, 2024</i>, and <b>proposals</b> are due by <i>April 10, 2024.</i>'
         ],
         buttonLabel: 'Contact Us',
       },
@@ -48,26 +51,22 @@ export function TagsMobile() {
     },
   ];
 
-  const [activeOption, setActiveOption] = useState("realEstate");
-  const [expandedOptions, setExpandedOptions] = useState<string[]>([
-    options[0].id,
-  ]);
-  const [showContactForm, setShowContactForm] = useState(false); 
-  const [contactButtonArrowDown, setContactButtonArrowDown] = useState(false); 
+  const [activeOption, setActiveOption] = useState('realEstate');
+  const [expandedOptions, setExpandedOptions] = useState<string[]>([options[0].id]);
+  const [activeContactForm, setActiveContactForm] = useState<string | null>(null);
 
   const handleOptionClick = (optionId: string) => {
-    // Toggle the selected option
     const newExpandedOptions = expandedOptions.includes(optionId)
       ? expandedOptions.filter((id) => id !== optionId)
       : [...expandedOptions, optionId];
 
     setActiveOption(optionId);
     setExpandedOptions(newExpandedOptions);
+    setActiveContactForm(null); // Cerrar el formulario de contacto cuando se cambia de panel
   };
 
-  const handleContactButtonClick = () => {
-    setShowContactForm((prevState) => !prevState); 
-    setContactButtonArrowDown((prevState) => !prevState); 
+  const handleContactButtonClick = (optionId: string) => {
+    setActiveContactForm(optionId === activeContactForm ? null : optionId); // Alternar el estado del formulario de contacto
   };
 
   return (
@@ -90,24 +89,30 @@ export function TagsMobile() {
                 <div>
                   <h2>{option.information.title}</h2>
                   {option.information.paragraphs.map((paragraph, index) => (
-                    <p key={index}>{paragraph}</p>
+                    <p
+                      key={index}
+                      dangerouslySetInnerHTML={{ __html: paragraph }}
+                    />
                   ))}
                 </div>
 
                 <button
                   className={css.buttonInformation}
-                  onClick={handleContactButtonClick}
+                  onClick={() => handleContactButtonClick(option.id)}
                 >
-                  {option.information.buttonLabel}
+                  {option.id === activeContactForm ? 'Close' : option.information.buttonLabel} {/* Cambiar la etiqueta del botón si el formulario está activo */}
                   <Image
                     alt="left arrow"
-                    src={contactButtonArrowDown ? "/assets/arrowDown.svg" : "/assets/leftArrowBLACK.svg"} 
+                    src={
+                      option.id === activeContactForm
+                        ? '/assets/arrowUp.svg' // Cambiar la flecha hacia arriba si el formulario está activo
+                        : '/assets/arrowDown.svg'
+                    }
                     width={13.207}
                     height={8.708}
                   />
                 </button>
-
-                {showContactForm && <ContactForm />} 
+                {option.id === activeContactForm && <ContactForm />} 
               </div>
             )}
           </div>
