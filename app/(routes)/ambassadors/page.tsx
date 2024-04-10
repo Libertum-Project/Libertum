c8944de4cbd65fc0299e46b51f0c8816ff00280b
nikiburgos <burgosveganicole@gmail.com>
2024-04-09 22:25:57 -0300

    why now  mobile and desktop

--- app/(routes)/ambassadors/page.tsx
+++ app/(routes)/ambassadors/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Hero } from "./hero/hero";
+import  WhatIs  from "./whatIs/WhatIs";
 
 export default function page() {
   return (
     <div>
       <Hero />
+      <WhatIs />
     </div>
   );
 }
---
+++ app/(routes)/ambassadors/whatIs/WhatIs.tsx
@@ -0,0 +1,42 @@
+import css from './WhatIs.module.css';
+import image from "./assets/image.svg";
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function WhatIs() {
+  return (
+    <div className={css.container}>
+      <div className={css.card}>
+        <div>
+          <div>
+            <h2>What is an Ambassador?</h2>
+            <p>
+              By becoming an Ambassador, you will assist us in spreading the
+              word about RWA tokenisation and our efforts in the Libertum
+              Ecosystem. Ambassadors have the power to host events, craft
+              content, and be part of the upcoming tokenisation wave.
+            </p>
+          </div>
+          <Link href="/community" className={css.applyButton}>
+            <p>Apply Now</p>
+            <Image
+              alt="left arrow"
+              src="/assets/leftArrowBLACK.svg"
+              width={13.207}
+              height={8.708}
+            />
+          </Link>
+        </div>
+
+        <div className={css.imageContainer}>
+          <Image
+            alt="image ambassadors"
+            src={image}
+            width={419}
+            height={367}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
