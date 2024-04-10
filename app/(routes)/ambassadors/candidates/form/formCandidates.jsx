f1a544fe5aa97371909fb1e98c0a277ffe8f0a74
nikiburgos <burgosveganicole@gmail.com>
2024-04-10 15:30:44 -0300

    form nodemailer

--- app/(routes)/ambassadors/candidates/form/formCandidates.jsx
+++ app/(routes)/ambassadors/candidates/form/formCandidates.jsx
@@ -1,45 +1,102 @@
+'use client';
+
+import { useState } from 'react';
 import css from './form.module.css';
 import Image from 'next/image';
 
 export function FormCandidates() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = (e) => {
+    setIsChecked(e.target.checked);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    if (!isChecked) {
+      alert('Please consent to receiving additional communications from Libertum.');
+      return;
+    }
+  
+    const formData = {
+      firstName: e.target.elements.firstName.value,
+      lastName: e.target.elements.lastName.value,
+      email: e.target.elements.email.value,
+      message: e.target.elements.message.value,
+    };
+  
+    try {
+      const response = await fetch('/api/ambassadors/route', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      if (response.ok) {
+        alert('Correo electrónico enviado exitosamente');
+      } else {
+        alert('Hubo un problema al enviar el correo electrónico');
+      }
+    } catch (error) {
+      console.error('Error al enviar la solicitud:', error);
+      alert('Hubo un problema al enviar la solicitud');
+    }
+  };
+
   return (
     <div className={css.formContainer}>
       <h2>Applications are now open!</h2>
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="email" name="" id="" placeholder="Email" />
-      <textarea
-        name=""
-        id=""
-        cols="30"
-        rows="10"
-        placeholder="Leave a message..."
-      ></textarea>
+      <form onSubmit={handleSubmit}>
+        <div className={css.formFrame}>
+          <input type="text" name="firstName" placeholder="First Name" />
+          <input type="text" name="lastName" placeholder="Last Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <textarea
+            name="message"
+            cols="30"
+            rows="10"
+            placeholder="Leave a message..."
+          ></textarea>
+        </div>
 
-      <div className={css.formFooter}>
-        <p>
-          At Libertum, we are dedicated to safeguarding and respecting your
-          privacy. Your personal information is used solely to manage your
-          account and deliver the requested products and services. Occasionally,
-          we would like to update you on our offerings and relevant content. If
-          you agree to this, please indicate your preferred contact method
-          below:
-        </p>
-        <label>
-        <input type="checkbox" name="checkbox1" value="option1" />
-          I consent to receiving additional communications from Libertum.
-       </label>
-        <p>
-          You have the option to opt out of these communications at any time. By
-          clicking submit, you authorise defactor.com to securely store and
-          process the provided personal data for the requested content.
-        </p>
-      </div>
+        <div className={css.formFooter}>
+          <p>
+            At Libertum, we are dedicated to safeguarding and respecting your
+            privacy. Your personal information is used solely to manage your
+            account and deliver the requested products and services.
+            Occasionally, we would like to update you on our offerings and
+            relevant content. If you agree to this, please indicate your
+            preferred contact method below:
+          </p>
+          <label>
+            <input
+              type="checkbox"
+              name="checkbox1"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
+            I consent to receiving additional communications from Libertum.
+          </label>
+          <p>
+            You have the option to opt out of these communications at any time.
+            By clicking submit, you authorize defactor.com to securely store and
+            process the provided personal data for the requested content.
+          </p>
+        </div>
 
-      <button className={css.buttonSubmit}>
-        Submit 
-        <Image src="/assets/arrowForm.svg" alt='submit' width={16} height={16}/>
-      </button>
+        <button type="submit" className={css.buttonSubmit}>
+          Submit{' '}
+          <Image
+            src="/assets/arrowForm.svg"
+            alt="submit"
+            width={16}
+            height={16}
+          />
+        </button>
+      </form>
     </div>
   );
 }
