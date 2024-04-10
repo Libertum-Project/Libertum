32082e4416f99a061c4b215555cac69708ca9887
nikiburgos <burgosveganicole@gmail.com>
2024-04-10 00:23:04 -0300

    whats included for ambassadors

---
+++ app/(routes)/ambassadors/included/Included.tsx
@@ -0,0 +1,53 @@
+import access from "../assets/access.svg";
+import merchandise from "../assets/merchandise.svg";
+import more from "../assets/more.svg";
+import preview from "../assets/previewroadmaps.svg";
+import remuneration from "../assets/remuneration.svg";
+import css from "./Included.module.css"
+import Image from "next/image";
+
+export function Included() {
+  const cards = [
+    {
+      title: 'Exclusive access to special events',
+      image: access
+    },
+    {
+      title: 'Remuneration',
+      image: remuneration
+    },
+    {
+      title: 'Preview roadmaps',
+      image: preview
+    },
+    {
+      title: 'Libertum merchandise',
+      image: merchandise
+    },
+    {
+      title: 'And more!',
+      image: more
+    }
+  ]
+
+  return (
+    <div className={css.container}>
+      <h2>What’s included for Ambassadors?</h2>
+      <div className={css.frame}>
+        {cards.map((card, index) => (
+          <div key={index} className={css.card}>
+            <div className={css.imageContainer}>
+              <Image
+                src={card.image}
+                alt={card.title}
+                width={64}
+                height={64}
+              />
+            </div>
+            <p>{card.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/(routes)/ambassadors/page.tsx
+++ app/(routes)/ambassadors/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Hero } from "./hero/hero";
 import  WhatIs  from "./whatIs/WhatIs";
+import { Included } from './included/Included';
 
 export default function page() {
   return (
     <div>
       <Hero />
       <WhatIs />
+      <Included />
     </div>
   );
 }
