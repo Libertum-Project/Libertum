ac5b8407a0a8b86c7ceb3543c5543cf247ee8c9a
nikiburgos <burgosveganicole@gmail.com>
2024-04-10 09:22:57 -0300

    join the team desktop

--- app/(routes)/ambassadors/joinTeam/JoinTeam.tsx
+++ app/(routes)/ambassadors/joinTeam/JoinTeam.tsx
@@ -1,8 +1,57 @@
 'use client';
 import { useState, useEffect } from 'react';
 import css from './JoinTeam.module.css';
+import alanPhoto from './assets/alan.svg';
+import javvadPhoto from './assets/javvad.svg';
+import discord from './assets/socialsIcons/discord.svg';
+import linkedin from './assets/socialsIcons/linkedin.svg';
+import telegram from './assets/socialsIcons/telegram.svg';
+import twitter from './assets/socialsIcons/twitter.svg';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export function JoinTeam() {
+  const ambassadors = [
+    {
+      name: 'Alan Gormley',
+      image: alanPhoto,
+      summary: 'Lorem ipsum dolor sit amet',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non nulla nisi. Curabitur interdum velit eu condimentum laoreet. Nulla gravida congue ultricies. Sed feugiat eu ipsum in feugiat. Cras sed fringilla est. Morbi velit torto.',
+      socials: {
+        linkedin: '/',
+        discord: '/',
+        telegram: '/',
+        twitter: '/',
+      },
+    },
+    {
+      name: 'Javvad Azam',
+      image: javvadPhoto,
+      summary: 'Lorem ipsum dolor sit amet',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non nulla nisi. Curabitur interdum velit eu condimentum laoreet. Nulla gravida congue ultricies. Sed feugiat eu ipsum in feugiat. Cras sed fringilla est. Morbi velit torto.',
+      socials: {
+        linkedin: '/',
+        discord: '/',
+        telegram: '/',
+        twitter: '/',
+      },
+    },
+    {
+      name: 'Alan Gormley',
+      image: alanPhoto,
+      summary: 'Lorem ipsum dolor sit amet',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non nulla nisi. Curabitur interdum velit eu condimentum laoreet. Nulla gravida congue ultricies. Sed feugiat eu ipsum in feugiat. Cras sed fringilla est. Morbi velit torto.',
+      socials: {
+        linkedin: '/',
+        discord: '/',
+        telegram: '/',
+        twitter: '/',
+      },
+    },
+  ];
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -42,20 +91,52 @@ export function JoinTeam() {
           and championing the future of finance through RWA tokenisation.
         </p>
         <div className={css.cardsContainer}>
-          <div className={css.card}>
-            image
-            <div>
-              <h4>name</h4>
-              <h5>summary</h5>
+          {ambassadors.map((ambassador, index) => (
+            <div key={index} className={css.card}>
+              <div className={css.imageContainer}>
+                <Image
+                  alt={ambassador.name}
+                  src={ambassador.image}
+                  width={120}
+                  height={120}
+                />
+              </div>
+
+              <div>
+                <h4>{ambassador.name}</h4>
+                <h5>{ambassador.summary}</h5>
+              </div>
+
+              <p>{ambassador.description}</p>
+
+              <div className={css.socialMediaFrame}>
+                <Link
+                  href={ambassador.socials.linkedin}
+                  className={css.socialMediaIcon}
+                >
+                  <Image alt="LinkedIn" src={linkedin} width={18} height={18} />
+                </Link>
+                <Link
+                  href={ambassador.socials.discord}
+                  className={css.socialMediaIcon}
+                >
+                  <Image alt="Discord" src={discord} width={18} height={18} />
+                </Link>
+                <Link
+                  href={ambassador.socials.telegram}
+                  className={css.socialMediaIcon}
+                >
+                  <Image alt="Telegram" src={telegram} width={18} height={18} />
+                </Link>
+                <Link
+                  href={ambassador.socials.twitter}
+                  className={css.socialMediaIcon}
+                >
+                  <Image alt="Twitter" src={twitter} width={18} height={18} />
+                </Link>
+              </div>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              non nulla nisi. Curabitur interdum velit eu condimentum laoreet.
-              Nulla gravida congue ultricies. Sed feugiat eu ipsum in feugiat.
-              Cras sed fringilla est. Morbi velit torto.
-            </p>
-            <div>social media</div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
