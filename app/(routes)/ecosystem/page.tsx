a57e3d4d055b93fa51fd728d8f1000ec6b7d1401
nikiburgos <burgosveganicole@gmail.com>
2024-04-11 16:27:07 -0300

    partners

--- app/(routes)/ecosystem/page.tsx
+++ app/(routes)/ecosystem/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Hero } from './hero/hero';
 import { BrandCarrousel } from './brandsCarrousel/BrandCarrousel';
+import Partners from './partners/Partners';
 
 export default function Ecosystem() {
   return (
     <div>
       <Hero />
       <BrandCarrousel />
+      <Partners />
     </div>
   );
 }
