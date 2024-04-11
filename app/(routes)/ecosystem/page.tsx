3e42cfc5cfa205f005a40be9bcd911c0b342fb45
nikiburgos <burgosveganicole@gmail.com>
2024-04-11 15:53:48 -0300

    ecosystem set up, hero and carrousel for  desktop

---
+++ app/(routes)/ecosystem/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hero } from './hero/hero';
+import { BrandCarrousel } from './brandsCarrousel/BrandCarrousel';
+
+export default function Ecosystem() {
+  return (
+    <div>
+      <Hero />
+      <BrandCarrousel />
+    </div>
+  );
+}
