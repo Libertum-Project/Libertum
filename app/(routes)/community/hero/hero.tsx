30e438d8eb1a0627e0cdd9c8e1c6efd8bea69477
madhava <galarzaguillermo.ggm@gmail.com>
2024-04-16 12:03:32 -0300

    Add DEXTools

--- app/(routes)/community/hero/hero.tsx
+++ app/(routes)/community/hero/hero.tsx
@@ -1,54 +1,52 @@
-'use client'
-
-import { type ReactElement,  useState, useEffect } from "react";
-import Image from "next/image";
-import css from "./hero.module.css";
-import Link from "next/link";
-import SocialMedia from "./socials/SocialMedia";
-import { ContactForm } from "./contactForm/contactFrom";
-
+'use client';
+import { type ReactElement, useState, useEffect } from 'react';
+import css from './hero.module.css';
+import SocialMedia from './socials/SocialMedia';
+import { ContactForm } from './contactForm/contactFrom';
 
 export function Hero(): ReactElement {
-
   const [isMobile, setIsMobile] = useState(false);
-  
+
   useEffect(() => {
-  const mediaQuery = window.matchMedia("(max-width: 950px)");
+    const mediaQuery = window.matchMedia('(max-width: 950px)');
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 950);
       setIsMobile(mediaQuery.matches);
     };
-    if (typeof window !== "undefined") {
-      window.addEventListener("resize", handleResize);
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', handleResize);
       handleResize();
 
       return () => {
-        window.removeEventListener("resize", handleResize);
+        window.removeEventListener('resize', handleResize);
       };
     }
   }, []);
 
   return (
     <div className={css.heroContainer}>
-      {isMobile ? 
-        <div className={css.backgroundNoVideo}></div> 
-        : 
+      {isMobile ? (
+        <div className={css.backgroundNoVideo}></div>
+      ) : (
         <video autoPlay muted loop className={css.video}>
           <source src="./bg-video-1.mp4" type="video/mp4" />
-        </video> 
-      }
+        </video>
+      )}
       <div className={css.heroCommunity}>
         <div className={css.textCommunity}>
           <h1>Community</h1>
           <div className={css.paragraphsCommunity}>
             <p>
-            Connect, engage, and grow with us on platforms like Discord and Telegram, where vibrant discussions and valuable insights bring our web3 real estate vision to life.            </p>          
+              Connect, engage, and grow with us on platforms like Discord and
+              Telegram, where vibrant discussions and valuable insights bring
+              our web3 real estate vision to life.{' '}
+            </p>
           </div>
-            <SocialMedia />
+          <SocialMedia />
         </div>
 
         <div>
-        <ContactForm/>
+          <ContactForm />
         </div>
       </div>
     </div>
--- app/(routes)/community/page.tsx
+++ app/(routes)/community/page.tsx
@@ -1,11 +1,11 @@
-import { Hero } from "./hero/hero"; 
-import { Feed } from "./feed/feed";
+import { Hero } from './hero/hero';
+import { Feed } from './feed/feed';
 
 export default function Community() {
-    return(
-        <>
-            <Hero />
-            <Feed />
-        </>
-    )
+  return (
+    <>
+      <Hero />
+      <Feed />
+    </>
+  );
 }
