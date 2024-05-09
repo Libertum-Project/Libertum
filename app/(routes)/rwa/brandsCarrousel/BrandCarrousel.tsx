ccce0ba2e8e4c26dd25f294ee93cbcca3bbc1715
nikiburgos <burgosveganicole@gmail.com>
2024-05-09 19:34:11 -0300

    fix(rwa): fixed with commit comments

--- app/(routes)/rwa/brandsCarrousel/BrandCarrousel.tsx
+++ app/(routes)/rwa/brandsCarrousel/BrandCarrousel.tsx
@@ -1,36 +1,41 @@
-'use client'
+'use client';
+import Marquee from 'react-fast-marquee';
 
-import { ReactElement, useEffect, useRef, useMemo, useState } from "react";
-import Image from "next/image";
-import css from "./BrandCarrousel.module.css";
-import aptos from "./brands/aptos.svg";
-import aws from "./brands/aws.svg";
-import coinFluence from "./brands/coinFluence.svg";
-import coinweb from "./brands/coinweb.svg";
-import googleForStartups from "./brands/googleforStatups.svg";
-import hotelCollection from "./brands/hotelCollection.svg";
-import hubspot from "./brands/Hubspot.svg";
-import leonmisk from "./brands/leonmiks.svg";
-import microsoft from "./brands/microsoft.svg";
-import plume from "./brands/plume.svg";
-import polygon from "./brands/polygon.svg";
-import realestateAgency from "./brands/realstateagency.svg";
+import { ReactElement } from 'react';
+import Image from 'next/image';
+import css from './BrandCarrousel.module.css';
+import aptos from './brands/aptos.svg';
+import aws from './brands/aws.svg';
+import coinFluence from './brands/coinFluence.svg';
+import coinweb from './brands/coinweb.svg';
+import googleForStartups from './brands/googleforStatups.svg';
+import hotelCollection from './brands/hotelCollection.svg';
+import hubspot from './brands/Hubspot.svg';
+import leonmisk from './brands/leonmiks.svg';
+import microsoft from './brands/microsoft.svg';
+import plume from './brands/plume.svg';
+import polygon from './brands/polygon.svg';
+import realestateAgency from './brands/realstateagency.svg';
 
 export function BrandCarrousel(): ReactElement {
-  const brands = useMemo(
-    () => [aptos, aws, coinFluence, googleForStartups, hotelCollection, hubspot, leonmisk, microsoft, plume, polygon, realestateAgency],
-    []
-  );
-
-  const combinedBrands = useMemo(
-    () => [...brands, ...brands, ...brands],
-    [brands]
-  );
+  const brands = [
+      aptos,
+      aws,
+      coinFluence,
+      googleForStartups,
+      hotelCollection,
+      hubspot,
+      leonmisk,
+      microsoft,
+      plume,
+      polygon,
+      realestateAgency
+    ];
 
   return (
     <div className={css.container}>
       <Marquee speed={50} pauseOnHover>
-        {combinedBrands.map((brand, index) => (
+        {brands.map((brand, index) => (
           <div key={index} className={css.slide}>
             <Image
               src={brand}
@@ -45,4 +50,3 @@ export function BrandCarrousel(): ReactElement {
     </div>
   );
 }
-
