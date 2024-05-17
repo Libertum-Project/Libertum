b66680e4a4b4d8667de7df32c7125a778c9eede7
nikiburgos <burgosveganicole@gmail.com>
2024-05-17 02:26:45 -0300

    feat(roadmap): added animations and tailwind to roadmap page

--- app/(routes)/roadmap/Hero.tsx
+++ app/(routes)/roadmap/Hero.tsx
@@ -1,10 +1,8 @@
 'use client';
 import { type ReactElement, useState, useEffect } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-
-import css from './hero.module.css';
-import backgroundImage from './background.svg';
+import { motion } from 'framer-motion';
+import { TypingText, TitleText } from '@/app/components/CustomTexts';
+import { fadeIn, staggerContainer } from '@/utils/motion';
 
 export function Hero(): ReactElement {
   const [isMobile, setIsMobile] = useState(false);
@@ -26,32 +24,40 @@ export function Hero(): ReactElement {
   }, []);
 
   return (
-    <div className={css.heroContainer}>
+    <div className="relative pt-[5rem] overflow-hidden">
       {isMobile ? (
-        <div className={css.backgroundNoVideo}></div>
+        <div className="heroGradient"></div>
       ) : (
-        <video autoPlay muted loop className={css.video}>
+        <video autoPlay muted loop className="heroVideo">
           <source src="./bg-video-1.mp4" type="video/mp4" />
         </video>
       )}
 
-      <div className={css.heroAmbassadors}>
-        <div className={css.textAmbassadors}>
-          <h3>LIBERTUM ROADMAP</h3>
-          <h1>
-            Libertum:
-            <br />
-            Doing things differently
-          </h1>
-          <div className={css.paragraphsAmbassadors}>
-            <p>
+      <div className="lg:max-w-containerWidth py-[7rem] px-4 mx-auto  flex flex-col items-center gap-16 relative z-50 box-border justify-center flex-1">
+        <div className="flex flex-col justify-center gap-4 items-center lg:w-[48rem] box-border ">
+          <TypingText title="LIBERTUM ROADMAP" textStyles="heroOvertitle" />
+          <TitleText
+            title="Libertum:
+            Doing things differently"
+            textStyles="heroTitle"
+          />
+          <motion.div
+            variants={staggerContainer()}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.25 }}
+          >
+            <motion.p
+              variants={fadeIn('up', 'tween', 1, 1)}
+              className="text-white font-montserrat lg:text-center font-normal"
+            >
               Libertum is an entire ecosystem designed to break down the barriers of traditional real estate and cater
               to a new generation of investors. 2014 is the year that Libertum differentiates itself from other RWA real
               estate projects! We go beyond tokenising real estate and trading on secondary markets. With $LBM at the
               centre of our ecosystem we are creating a circular economy where token holders can not only invest and
               earn in properties, they will be able to live work and play in them as well. 
-            </p>
-          </div>
+            </motion.p>
+          </motion.div>
         </div>
       </div>
     </div>
--- app/(routes)/roadmap/page.tsx
+++ app/(routes)/roadmap/page.tsx
@@ -1,87 +1,33 @@
+'use client'
 import React from 'react';
 
-import { Hero } from './hero/hero';
-import { Feed } from './feed/feed';
+
+import FeedTimeLine from './feedTimeline/FeedTimeline';
+import { Hero } from './Hero';
+import { motion } from 'framer-motion';
+import { staggerContainer, fadeIn } from '@/utils/motion';
 
 export default function Roadmap() {
-  7;
-  const items = [
-    {
-      title: 'Q1',
-      date: '- 2024',
-      paragraphs: [
-        'Libertum Market Launch',
-        'Secondary Marketplace Launch v1.0',
-        'Ambassador Program Begins',
-        'Launch on Plume Test Net',
-        'Introduce Social Login and Account Abstraction',
-        'Regulatory Compliance',
-        '$LBM Public Sale',
-        ' Solid Proof Audits',
-      ],
-    },
-    {
-      title: 'Q2',
-      date: '- 2024',
-      paragraphs: [
-        ' 1st Tokenized Property and Rental Rewards Begins',
-        'Expand Properties (Dubai, Mexico, Brazil & the EU) 10 -15',
-        'Launch Secondary Marketplace ',
-        'Launch “View” Libertums Token Analytics and Staking Poolsl',
-        ' Launch on CoinWeb Cross Chain Test  Infrastructure',
-        '2xDEX listings',
-        ' Launch of “Get” Liquidity and Swap Tool',
-      ],
-    },
-    {
-      title: 'Q3',
-      date: '- 2024',
-      paragraphs: [
-        'Lending and Borrowing Protocol Launched',
-        'Launch  “Choose” - our Governance and Voting Protocol',
-        'Introduce a Second Tokenisation Methodology',
-        'Secondary Market Supports 3rd Party Properties',
-        ' STO Licence x 2',
-        'Tokenization of Parking Lots',
-        ' Introduction of “Stay” - On-chain AirBnB for Token Holders',
-      ],
-    },
-    {
-      title: 'Q4',
-      date: '- 2024',
-      paragraphs: [
-        'Global Expansion',
-        'CEX Launches',
-        ' Community Owned Digital Nomad Properties ',
-        'Expand Blockchain Networks',
-        ' Gamification of Marketplace',
-        'White Labelled Solution for Real Estate Companies and RWA Partners',
-      ],
-    },
-    {
-      title: '2025',
-      paragraphs: [
-        'Libertums AI Property Valuation Tool',
-        'Enhanced Token Utilities',
-        'Launch Libertums Grant Scheme',
-        'Dubai Property for Events and Remote Office for Token Holders',
-        ' Libertums AI investment Strategy Tool',
-      ],
-    },
-  ];
   return (
     <>
       <Hero />
-      <div className="container  max-w-[75rem] m-auto flex flex-col justify-center items-center mt-12">
-        <p className="font-space_grotesk font-semibold text-4xl ">Roadmap</p>
+      <motion.div variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }} className="container  max-w-[75rem] m-auto flex flex-col justify-center items-center mt-12">
 
-        <video controls width="600" className="rounded-[15px] mt-6" poster="./roadmapView.png">
+        <motion.p 
+        className="font-space_grotesk font-semibold text-4xl"
+        variants={fadeIn('up', 'tween', 1.4, 1)}
+        >Roadmap
+        </motion.p>
+
+        <motion.video variants={fadeIn('up', 'tween', 1.6, 1)} controls width="600" className="rounded-[15px] mt-6" poster="./roadmapView.png">
           <source src="/LBMRoadmap.mp4" type="video/mp4" />
           Your browser does not support the video tag.
-        </video>
-      </div>
-
-      <Feed items={items} />
+        </motion.video>
+      </motion.div>      
+      <FeedTimeLine />
     </>
   );
 }
