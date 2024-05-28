a0e8fccc0d9a69a2694c72f69a1b087a0b7895de
madhava <galarzaguillermo.ggm@gmail.com>
2024-05-28 16:55:59 -0300

    chore: no-duplicate-imports

--- app/components/Navbar/MobileNavbar/MobileNavbar.tsx
+++ app/components/Navbar/MobileNavbar/MobileNavbar.tsx
@@ -1,6 +1,5 @@
 'use client';
-import { type ReactElement } from 'react';
-import { useState } from 'react';
+import { type ReactElement, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
