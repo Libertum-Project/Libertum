1c454adb14c6b241abff52cfa4aeda4f26c96fbb
nikiburgos <burgosveganicole@gmail.com>
2024-06-23 02:05:31 -0300

    chore: installed react-spline and added to hero, changed css to tailwind- [WEB-59]

---
+++ app/(routes)/(landingpage)/HeroSpline.tsx
@@ -0,0 +1,5 @@
+import Spline from '@splinetool/react-spline/next';
+
+export function HeroSplineLanding() {
+  return <Spline scene="https://prod.spline.design/1YVyhj7Fm5h7mO5Y/scene.splinecode" />;
+}
